cfc8e1cc91fea86b3137f9dc93bc0c9342cfba81
Dheeraj Kulkarni <dheeraj.kulkarni@infeon.in>
2022-03-29 09:19:41 +0530

    sidenav responsive link added

--- src/assets/reference_links.ts
+++ src/assets/reference_links.ts
@@ -1,3 +1,5 @@
 // https://www.npmjs.com/package/@auth0/angular-jwt
 // https://www.npmjs.com/package/ngx-toastr
 //https://www.c-sharpcorner.com/article/client-side-application-for-jwt-refresh-token-in-angular-13/
+
+//https://stackblitz.com/angular/rmjxrpxdgmx?file=app%2Fsidenav-responsive-example.html
