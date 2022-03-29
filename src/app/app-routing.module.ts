a8b5c2671504f1aa2e641062f2372a9b8c68cbca
Dheeraj Kulkarni <dheeraj.kulkarni@infeon.in>
2022-03-29 09:52:03 +0530

    bootstrap grid with responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './registration/login/login.component';
 import { SignUpComponent } from './registration/sign-up/sign-up.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path:'login', component:LoginComponent},
   {path:'sign-up',component:SignUpComponent},
+  {path:'dashboard',component:DashboardComponent},
   {
     path:'', pathMatch:'full', redirectTo:'/sign-up',
   }
